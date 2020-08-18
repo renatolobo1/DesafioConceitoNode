086734e29f5b2332f04c6b1227bb228248526956
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-17 21:05:37 -0300

    Versão 3 da resposta

--- src/app.js
+++ src/app.js
@@ -9,59 +9,74 @@ app.use(cors());
 
 const repositories = [];
 
+// function validateRepositoieId(request, response, next){
+//   const { id} = request.params;
 
-app.get('/repositories', (request, response) => { 
-  const { title, url, techs } = request.body;
+//   if(!isUuid(id)){
+//     return response.status(400).json({ error: 'Invalid Project ID'});
+//   }
+//   return next();
+// }
+
+  // app.get('/repositories', (request, response) => { 
+  //   const {title} = request.query;
   
-  const results = title
-    ? repositories.filter(repository => repository.title.include(title))
-    : repositories;
+  //   const results = title
+  //     ? repositories.filter(repositorie => repositorie.title.includes(title))
+  //     : repositories
+  
+  //   return response.json(results);
+  // });
 
-  return response.json(results);
-});
+  app.get('/repositories', (request, response) => { 
+    
+  
+    return response.json(repositories);
+  });
+
 
 app.post("/repositories", (request, response) => {
-  const {title, url, techs, likes} = request.body;
+  const {title, url, techs} = request.body;
 
-  const repositorie = {id: uuid(),title, url, techs, likes};
+  const repository = {id: uuid(),title, url, techs, likes:0};
 
-  repositories.push(repositorie);
+  repositories.push(repository);
 
-  return response.json(repositorie);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const {id} = request.params;
   const {title, url, techs} = request.body;
 
-  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id );
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
 
-  if (repositorieIndex < 0) {
+  if (repositoryIndex < 0) {
     return response.status(400).json({ error: 'Project not found.'})
   }
 
-  const repositorie = {
+  const repository = {
     id,
     title, 
     url, 
     techs
   };
 
-  repositories[repositorieIndex] = repositorie;
+  repositories[repositoryIndex] = repository;
 
-  return response.json(repositorie);
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const {id} = request.params;
 
-  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id );
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
 
-  if (repositorieIndex < 0) {
+  if (repositoryIndex < 0) {
     return response.status(400).json({ error: 'Project not found.'})
   }
 
-  repositories.splice(repositorieIndex, 1);
+  repositories.splice(repositoryIndex, 1);
 
   return response.status(204).send();
   
